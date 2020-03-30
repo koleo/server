6fa4541802e68b881b61f6d689251551ce748224
Nikita Malyavin <nikitamalyavin@gmail.com>
2020-03-30 15:19:01 +1000

    lol we re close

--- sql/handler.cc
+++ sql/handler.cc
@@ -6309,6 +6309,9 @@ static int binlog_log_row_online_alter(TABLE* table,
   if (!table->online_alter_cache)
     table->online_alter_cache= thd->binlog_setup_cache_data(&table->mem_root);
 
+  // We need to log all columns for the caase if alter table chanes primary key
+  table->use_all_columns();
+
   // this value can be important during error handling when flushing
   // see flush_and_set_pending_rows_event and set_write_error of MYSQL_BIN_LOG
   bool has_trans= table->file->has_transactions() ||
