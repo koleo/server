6ed632bb6e91993b9a1f408b5d807c3c45459ba1
Oleksandr Byelkin <sanja@mariadb.com>
2020-05-18 15:55:23 +0200

    MENT-447: MTR Bug test "main.mysql_client_test_nonblock" is failing in the MTR Azure Pipeline
    In case of bintar with unsuported plugins collect them for tests.

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2656,10 +2656,11 @@ sub setup_vardir() {
   # and make them world readable
   copytree("$glob_mysql_test_dir/std_data", "$opt_vardir/std_data", "0022");
 
+  # create a plugin dir and copy or symlink plugins into it
   unless($plugindir)
   {
-    # create a plugin dir and copy or symlink plugins into it
-    if ($source_dist)
+    my $bintar= -d  "$bindir/share/not_supported/";
+    if ($source_dist or $bintar)
     {
       $plugindir="$opt_vardir/plugins";
       # Source builds collect both client plugins and server plugins in the
@@ -2695,6 +2696,8 @@ sub setup_vardir() {
              <$bindir/plugin/*/auth_pam_tool_dir>,
              <$bindir/libmariadb/plugins/*/*.so>,
              <$bindir/libmariadb/*.so>,
+             <$bindir/lib/plugin/*.so>,             # bintar
+             <$bindir/share/not_supported/*.so>,    # bintar
              <$bindir/sql/*.so>)
         {
           my $pname=basename($_);
